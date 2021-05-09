2a69ca274ab32a7113e71c537ca32791bb0b9625
olivia-pham <62281454+olivia-pham@users.noreply.github.com>
2021-05-09 19:50:14 +1000

    Update Assignment-3.cpp

--- Assignment 3/Assignment-3.cpp
+++ Assignment 3/Assignment-3.cpp
@@ -1,1 +1,86 @@
+//===- SVF-Teaching Assignment 3-------------------------------------//
+//
+//    SVF: Static Value-Flow Analysis Framework for Source Code       
+//
+// Copyright (C) <2013->
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===-----------------------------------------------------------------------===//
+
+/*
+ // Assignment 3 : Andersen's pointer analysis
+ //
+ // 
+ */
+
+#include "SVF-FE/LLVMUtil.h"
+#include "SVF-FE/PAGBuilder.h"
+#include "WPA/Andersen.h"
+#include "Assignment-3.h"
+using namespace SVF;
+using namespace llvm;
+using namespace std;
+
+
+// TODO: Implement your Andersen's Algorithm here
+void AndersenPTA::solveWorklist(){
+        processAllAddr();	
+	// TODO: Andersen's worklist-based transitive closure solving starts from here
+    while (!isWorklistEmpty()){
+        NodeID nodeId = popFromWorklist();   
+        ConstraintNode* node = consCG->getConstraintNode(nodeId);
+                               
+        for (PointsTo::iterator piter = getPts(nodeId).begin(), epiter =
+                getPts(nodeId).end(); piter != epiter; ++piter){   
+            NodeID ptd = *piter;  
+            for (ConstraintEdge* e : node->getStoreInEdges()){ //store rule 
+                if (NodeID src = e->getSrcID())
+                    addCopyEdge(src, ptd); 
+                    pushIntoWorklist(e->getSrcID());
+            }
+
+            for (ConstraintEdge* e : node->getLoadOutEdges()){ //load rule 
+                if (NodeID dst = e->getDstID())                    
+                    addCopyEdge(ptd, dst); 
+                    pushIntoWorklist(ptd);
+            }
+        }
+        
+        for (ConstraintEdge* e : node->getDirectOutEdges()){ //copy rule
+            NodeID dst = e->getDstID();
+            const PointsTo& srcPts = getPts(nodeId);
+            bool changed = unionPts(dst, srcPts);
+            if (changed)
+                pushIntoWorklist(dst);
+        }  
+    }
+}
+
+// Process all address constraints to initialize pointers' points-to sets
+void AndersenPTA::processAllAddr(){
+    for (ConstraintGraph::const_iterator nodeIt = consCG->begin(), nodeEit = consCG->end(); nodeIt != nodeEit; nodeIt++)
+    { 
+        ConstraintNode *cgNode =nodeIt->second; 
+        for (ConstraintEdge* edge : cgNode->getAddrInEdges()) {
+            const AddrCGEdge *addr = SVFUtil::cast<AddrCGEdge>(edge);
+            NodeID dst = addr->getDstID();
+            NodeID src = addr->getSrcID();
+            if (addPts(dst, src))
+                pushIntoWorklist(dst);
+        } 
+    }
+}
 
